d5007e06f5f975b2aa4bcbe9f3bf2b67676f4342
Yash Jagdale <jagdale0210@gmail.com>
2023-01-19 21:24:18 +0530

    output added

--- non-prod/us-east-1/qa/vpc/outputs.tf
+++ non-prod/us-east-1/qa/vpc/outputs.tf
@@ -5,3 +5,31 @@ resource "aws_ssm_parameter" "vpc-id" {
   depends_on = [module.vpc]
   tags       = var.tags
 }
+
+resource "aws_ssm_parameter" "private-subnets" {
+  name       = "/${var.environment_name}/network/private_subnets"
+  type       = "StringList"
+  value      = join(",", module.vpc.private_subnets)
+  depends_on = [module.vpc]
+  tags       = var.tags
+}
+
+resource "aws_ssm_parameter" "public-subnets" {
+  name       = "/${var.environment_name}/network/public_subnets"
+  type       = "StringList"
+  value      = join(",", module.vpc.public_subnets)
+  depends_on = [module.vpc]
+  tags       = var.tags
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
